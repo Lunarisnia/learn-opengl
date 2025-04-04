23e6186bfda43c67dd24586327f8118d99ac0f2f
Lounarisnia <r.arswendo.r@gmail.com>
2025-04-04 15:49:05 +0700

    feat: windowing done

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 int main() {
+
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -17,5 +18,19 @@ int main() {
     return -1;
   }
   glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
