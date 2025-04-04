e7b7f6a30edc0f2d43d2f2aae91ca3bdd791a9fd
Lounarisnia <r.arswendo.r@gmail.com>
2025-04-04 15:06:52 +0700

    feat: setup the makefile to link library proper

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 run:
-	clang++ ./main.cpp -o debug
+	# Solution to linking found here: https://stackoverflow.com/questions/73142912/glfw-undefined-symbols-for-architecture-arm64
+	clang++ ./main.cpp -I/usr/local/include -L/usr/local/lib -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit -o debug
 	./debug
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#include <glad/glad.h>
+// FORCE
 #include <GLFW/glfw3.h>
-/*#include <glad/glad.h>*/
-#include <iostream>
 
 int main() {
-  std::cout << "Hello" << '\n';
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  /*glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);*/
+
   return 0;
 }
