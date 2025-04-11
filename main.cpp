1275e93cb8478e57afb8f18fba34c9302edaa3e6
Lounarisnia <r.arswendo.r@gmail.com>
2025-04-11 11:06:07 +0700

    feat: ebo with attribute

--- main.cpp
+++ main.cpp
@@ -43,18 +43,20 @@ int main() {
 
   // Vertices of a triangle
   float vertices[] = {
-      0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f, // top right
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, // top right
       0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
-      -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f  // bottom left
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 1.0f  //
+  };
+  int indices[] = {
+      0, 1, 3, //
+      1, 2, 3  //
   };
 
   Shader shaderProgram("./shaders/vertex-shader.vert",
                        "./shaders/fragment-shader.frag");
   shaderProgram.use();
-  shaderProgram.setFloat("horizontalOffset", 0.5f);
-
-  Shader shaderProgram2("./shaders/inverted-vertex.vert",
-                        "./shaders/fragment-shader.frag");
+  shaderProgram.setFloat("horizontalOffset", 0.0f);
 
   // We create a vertex buffer
   unsigned int VBO, VAO, EBO;
@@ -70,30 +72,13 @@ int main() {
   // Actually set the value of the vertex buffer on the GPU from the CPU
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
-                        (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-
-  float vertices2[] = {
-      1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, // Top right
-      1.0f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // Bottom right
-      0.6f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // Bottom left
-  };
-  unsigned int VBO2, VAO2;
-  glGenVertexArrays(1, &VAO2);
-  glGenBuffers(1, &VBO2);
-  glBindVertexArray(VAO2);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
-
-  // Position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  // Color Attribute
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
                         (void *)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
@@ -105,11 +90,29 @@ int main() {
   std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes
             << std::endl;
 
-  // Load Textures
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  // Set texture filtering
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // Load & Generate Textures
   // =============
   int width, height, nrChannels;
   unsigned char *data =
       stbi_load("./textures/container.jpg", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "ERROR::IMAGE_LOADING: failed to load textures" << std::endl;
+  }
+  stbi_image_free(data);
 
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
@@ -122,14 +125,10 @@ int main() {
 
     shaderProgram.use();
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    shaderProgram2.use();
-    glBindVertexArray(VAO2);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    /*glDrawArrays(GL_TRIANGLES, 0, 3);*/
 
     /*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);*/
-    /*glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
--- shaders/fragment-shader.frag
+++ shaders/fragment-shader.frag
@@ -9,4 +9,5 @@ void main() {
     FragColor = vec4(vertexColor, 1.0);
     FragColor = vec4(ourColor, 1.0);
     FragColor = vec4(vertexPosition, 1.0);
+    // FragColor = vec4(vec3(1.0), 1.0);
 }
